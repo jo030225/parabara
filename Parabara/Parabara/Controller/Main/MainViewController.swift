139035ed9959a6ad11e4dbdc9e80d272ac473544
jo030225 <whwngur123@naver.com>
2021-06-15 16:11:56 +0900

    [CREATE] 초기 파일 생성

---
+++ Parabara/Parabara/Controller/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Parabara
+//
+//  Created by 조주혁 on 2021/06/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

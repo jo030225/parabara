fb17d08b693b408273b67da29df214a2b34a10f0
jo030225 <whwngur123@naver.com>
2021-06-17 09:21:44 +0900

    [CREATE] DeleteViewController 생성

---
+++ Parabara/Parabara/Controller/Delete/DeleteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteViewController.swift
+//  Parabara
+//
+//  Created by 조주혁 on 2021/06/17.
+//
+
+import UIKit
+
+class DeleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}

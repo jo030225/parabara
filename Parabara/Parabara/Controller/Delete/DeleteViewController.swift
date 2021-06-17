9b940fd3442eb1d151b9d20fdd1355c02dd31e25
jo030225 <whwngur123@naver.com>
2021-06-17 10:09:41 +0900

    [ADD] UI 구성 및 연결

--- Parabara/Parabara/Controller/Delete/DeleteViewController.swift
+++ Parabara/Parabara/Controller/Delete/DeleteViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class DeleteViewController: UIViewController {
 
+    @IBOutlet weak var deleteTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setting()
+    }
+    
+    func setting() {
+        deleteTextField.delegate = self
+        deleteTextField.keyboardType = .numberPad
+    }
+    
+    @IBAction func deleteButton(_ sender: UIButton) {
         
+    }
+    
+    
+}
+
+extension DeleteViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        deleteTextField.resignFirstResponder()
     }
 }

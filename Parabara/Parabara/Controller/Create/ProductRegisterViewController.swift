30b9ec77f4bf52ce38ae420a71feec553dc0956f
jo030225 <whwngur123@naver.com>
2021-06-18 15:06:26 +0900

    [CREATE] ProductRegisterViewController 생성

---
+++ Parabara/Parabara/Controller/Create/ProductRegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProductRegisterViewController.swift
+//  Parabara
+//
+//  Created by 조주혁 on 2021/06/18.
+//
+
+import UIKit
+
+class ProductRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}

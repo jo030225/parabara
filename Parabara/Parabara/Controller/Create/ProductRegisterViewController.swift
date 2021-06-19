a2b313ceb2982d875433519d4dc6492e9a927cb0
jo030225 <whwngur123@naver.com>
2021-06-19 16:13:51 +0900

    [ADD] UI 구현 및 연결

--- Parabara/Parabara/Controller/Create/ProductRegisterViewController.swift
+++ Parabara/Parabara/Controller/Create/ProductRegisterViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ProductRegisterViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var priceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        setting()
+    }
+    
+    func setting() {
+        contentTextView.layer.borderWidth = 0.5
+        contentTextView.layer.cornerRadius = 5
+        
+        titleTextField.delegate = self
+        priceTextField.delegate = self
+        contentTextView.delegate = self
+    }
+}
+
+extension ProductRegisterViewController: UITextFieldDelegate, UITextViewDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }

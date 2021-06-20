d167afb6786ba4ca1709d5ab301edf3fe7e3eaa9
jo030225 <whwngur123@naver.com>
2021-06-20 17:49:29 +0900

    [CREATE] UpdateViewController,ContentViewController 생성

---
+++ Parabara/Parabara/Controller/Update/ContentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ContentViewController.swift
+//  Parabara
+//
+//  Created by 조주혁 on 2021/06/20.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Parabara/Parabara/Controller/Update/UpdateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpdateViewController.swift
+//  Parabara
+//
+//  Created by 조주혁 on 2021/06/20.
+//
+
+import UIKit
+
+class UpdateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}

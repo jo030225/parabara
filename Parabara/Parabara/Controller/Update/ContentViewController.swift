19349ebf056582d3b18357c94e8ec0ac7317655b
jo030225 <whwngur123@naver.com>
2021-06-21 11:55:05 +0900

    [ADD] 상품 내용 화면 구현

--- Parabara/Parabara/Controller/Update/ContentViewController.swift
+++ Parabara/Parabara/Controller/Update/ContentViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class ContentViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setting()
+    }
+    
+    func setting() {
+        titleLabel.text = ContentManager.getProductTitle()
+        contentLabel.text = ContentManager.getProductContent()
+        priceLabel.text = "\(ContentManager.getProductPrice())원"
+        idLabel.text = "\(ContentManager.getProductId())"
     }
 }

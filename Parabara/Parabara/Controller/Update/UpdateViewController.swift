01363dfe1291a36f09bc892d6e512def932c6643
jo030225 <whwngur123@naver.com>
2021-06-21 13:42:58 +0900

    [ADD] 상품 수정 화면 구현

--- Parabara/Parabara/Controller/Update/UpdateViewController.swift
+++ Parabara/Parabara/Controller/Update/UpdateViewController.swift
@@ -9,8 +9,32 @@ import UIKit
 
 class UpdateViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var priceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+        setting()
+    }
+    
+    func setting() {
+        titleTextField.delegate = self
+        contentTextView.delegate = self
+        priceTextField.delegate = self
+        
+        contentTextView.layer.borderWidth = 0.5
+        contentTextView.layer.cornerRadius = 5
+        
+        titleTextField.text = ContentManager.getProductTitle()
+        contentTextView.text = ContentManager.getProductContent()
+        priceTextField.text = "\(ContentManager.getProductPrice())"
+    }
+}
 
+extension UpdateViewController: UITextFieldDelegate, UITextViewDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 }
